0c160484b51e0444294ed8e71f46300cb75bc497
its-ar <armaneffendy003@gmail.com>
2023-07-31 14:12:18 +0700

    fix: react state

--- gen-23.05-react-state/src/component/Card.jsx
+++ gen-23.05-react-state/src/component/Card.jsx
@@ -1,24 +1,30 @@
-import pink from "../../public/img/pink.jpg";
-// import keranjang from "../../public/img/keranjang.jpg";
-export default function Card() {
+import { useState } from "react";
+import pink from "../assets/img/pink.jpg";
+import pinkAwal from "../assets/img/pinkAwal.jpg";
+import pinkSamping from "../assets/img/pinkSamping.jpg";
+import pinkTutup from "/src/assets/img/pinkTutup.jpg";
+
+const Card = () => {
+  const [gambarAwal, setGambar] = useState(pinkAwal);
+  const [angkaAwal, setAngka] = useState(1);
   return (
     <section className="flex my-8 mx-12 container m-3 lg:px-12 ">
       <div className="flex flex-col w-2/6 m-3 shadow-md ">
         <div className="flex flex-col bg-gray-100 justify-center items-center ">
-          <img src={pink} alt={pink} className="w-auto h-96 hover:scale-110 transition-transform duration-500 ease-in-out" />
+          <img src={gambarAwal} alt={pink} className="w-auto h-96 hover:scale-110 transition-transform duration-500 ease-in-out" />
         </div>
         <div className="flex flex-row gap-4 py-2 my-2 justify-between items-center">
           <div className="flex gap-2 h-24 w-auto bg-gray-100 py-2 m-2">
-            <img src={pink} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
+            <img onClick={() => setGambar(pinkAwal)} src={pinkAwal} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
           </div>
           <div className="flex gap-2 h-24 w-auto bg-gray-100 py-2 m-2">
-            <img src={pink} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
+            <img onClick={() => setGambar(pinkSamping)} src={pinkSamping} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
           </div>
           <div className="flex gap-2 h-24 w-auto bg-gray-100 py-2 m-2">
-            <img src={pink} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
+            <img onClick={() => setGambar(pinkTutup)} src={pinkTutup} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
           </div>
           <div className="flex gap-2 h-24 w-auto bg-gray-100 py-2 m-2">
-            <img src={pink} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
+            <img onClick={() => setGambar(pink)} src={pink} alt={pink} className="w-auto h-20 hover:scale-110 transition-transform duration-500 ease-in-out" />
           </div>
         </div>
       </div>
@@ -42,9 +48,13 @@ export default function Card() {
         </div>
         <div className="flex flex-row shadow-sm mt-3">
           <div className="flex flex-row w-24 justify-between px-3 py-1 border border-gray-400 items-center rounded-full bg-gray-200">
-            <button className="w-4 h-4 flex items-center justify-center text-green-700 rounded-full">-</button>
-            <p className="text-green-700">1</p>
-            <button className="w-4 h-4 flex items-center justify-center text-green-700 rounded-full">+</button>
+            <button onClick={() => setAngka(angkaAwal - 1)} className="w-4 h-4 flex items-center justify-center text-green-700 rounded-full">
+              -
+            </button>
+            <p className="text-green-700">{angkaAwal}</p>
+            <button onClick={() => setAngka(angkaAwal + 1)} className="w-4 h-4 flex items-center justify-center text-green-700 rounded-full">
+              +
+            </button>
           </div>
           <p className="ml-5">
             Only <span className="text-orange-500">12 items</span> Left
@@ -59,4 +69,5 @@ export default function Card() {
       </div>
     </section>
   );
-}
+};
+export default Card;
