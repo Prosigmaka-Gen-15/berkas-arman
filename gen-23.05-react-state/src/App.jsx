c102072208b83998c8cc9d669cc36b4f8bb57beb
its-ar <armaneffendy003@gmail.com>
2023-07-31 09:00:31 +0700

    belum usestate

---
+++ gen-23.05-react-state/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import "./App.css";
+import Card from "./component/Card";
+import Content from "./component/Content";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Content />
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ gen-23.05-react-state/src/component/Content.jsx
@@ -0,0 +1,7 @@
+export default function Content() {
+  return (
+    <div>
+      <h1>Electronics / Audio / Headphones / Shop Headphones by type / airpods-max</h1>
+    </div>
+  );
+}
