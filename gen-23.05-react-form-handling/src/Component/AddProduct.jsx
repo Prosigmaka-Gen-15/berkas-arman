2a76c73f255c2ee1a49a65eaeeefb14f4376f2d3
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-08-07 15:10:00 +0700

    fix: form modal

--- gen-23.05-react-form-handling/src/Component/AddProduct.jsx
+++ gen-23.05-react-form-handling/src/Component/AddProduct.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 
-const AddProduct = () => {
-  const [nama, setNama] = useState("arman");
-  return (
-    <div>
-      <br />
-      nama:
-      <input type="text" value={nama} placeholder="Nama" onChange={(event) => setNama(event.target.value)} />
-      <br />
-      {nama}
-    </div>
-  );
+const AddProduct = ({ setFormIsOpen }) => {
+  return <>
+    <section className="fixed inset-0 flex justify-center items-center">
+      <div onClick={() => setFormIsOpen(false)} className="bg-black/50 absolute w-full h-full"></div>
+      <form className="block relative w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
+        <h1>Contoh Form</h1>
+      </form>
+    </section>
+  </>
 };
 
 export default AddProduct;
